c32e42c363bd80544c856135760a2ece3ef6cc33
Joel Denning <joeldenning@gmail.com>
2024-06-21 16:19:21 -0600

    Initial setup

---
+++ src/spauna-cli.ts
@@ -0,0 +1,12 @@
+import yargs, { Arguments } from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+yargs(hideBin(process.argv))
+  .command('serve', 'start the server', (yargs) => {
+    return yargs.option('port', {
+      type: 'number'
+    })
+  }, (argv: Arguments) => {
+    console.log('time to serve', argv)
+  })
+  .parse();
