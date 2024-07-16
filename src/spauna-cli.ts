b0474741a3f6a35a956b0c0f6e4d98e89646399e
Joel Denning <joeldenning@gmail.com>
2024-07-16 15:03:18 -0600

    Implement fetchConfig function (#2)
    * Implement fetchConfig function
    
    * Ian's feedback

--- src/spauna-cli.ts
+++ src/spauna-cli.ts
@@ -1,17 +1,23 @@
 import yargs, { Arguments } from "yargs";
 import { hideBin } from "yargs/helpers";
+import { runCommand } from "./spauna.js";
 
 yargs(hideBin(process.argv))
   .command(
     "serve",
     "start the server",
     (yargs) => {
-      return yargs.option("port", {
-        type: "number",
-      });
+      return yargs
+        .option("port", {
+          type: "number",
+        })
+        .option("config", {
+          type: "string",
+        })
+        .demandOption("config");
     },
-    (argv: Arguments) => {
-      console.log("time to serve", argv);
+    async (argv: Arguments) => {
+      await runCommand("serve", argv.config as string, argv);
     },
   )
   .parse();
--- src/spauna.ts
+++ src/spauna.ts
@@ -1,5 +1,8 @@
 // See https://stackoverflow.com/a/74112582
 /// <reference lib="dom" />
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+import { pathToFileURL } from "node:url";
 
 export async function runCommand(
   commandName: string,
@@ -7,18 +10,64 @@ export async function runCommand(
   args: object,
 ) {
   // Step 1: Fetch Config
+  const config = await fetchConfig(
+    configLocation,
+    pathToFileURL(process.cwd() + "/"),
+  );
+
+  console.log(config);
   // Step 2: Find Command
   // Step 3: Execute Command
 }
 
-async function fetchConfig(configLocation: string) {
+interface SpaunaConfig {
+  extends?: string;
+}
+
+async function fetchConfig(
+  configLocation: string,
+  parentUrl: URL,
+): Promise<SpaunaConfig> {
+  let config: SpaunaConfig;
+  let extendParent: URL = parentUrl;
+
   if (
     configLocation.startsWith("http://") ||
     configLocation.startsWith("https://")
   ) {
     const configResult: Response = await fetch(configLocation);
     if (configResult.ok) {
-      const json = await configResult.json();
+      config = await configResult.json();
     }
+  } else if (
+    configLocation.startsWith("./") ||
+    configLocation.startsWith("../") ||
+    configLocation.startsWith("file://")
+  ) {
+    const fileUrl = new URL(configLocation, parentUrl);
+
+    extendParent = fileUrl;
+
+    const fileContents: string = await readFile(fileUrl, "utf-8");
+
+    config = JSON.parse(fileContents);
+  } else {
+    throw Error(
+      `spauna config location must start with with one of the following prefixes: ["https://", "http://", "./", "../", "file://"], but found ${configLocation} referenced in ${parentUrl}`,
+    );
   }
+
+  let finalConfig: SpaunaConfig;
+
+  if (config.extends) {
+    const otherConfig = await fetchConfig(config.extends, extendParent);
+
+    finalConfig = { ...otherConfig, ...config };
+  } else {
+    finalConfig = config;
+  }
+
+  delete finalConfig.extends;
+
+  return finalConfig;
 }
